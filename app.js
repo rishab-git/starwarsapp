1e41a401bf22f04a6e212f6ab21008a9f6231b3e
Rishabh Shukla <rishab.akg@gmail.com>
2020-06-27 20:23:31 +0530

    deploying to Heroku

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var path = require('path');
 var app = express();
+var sslRedirect = require('heroku-ssl-redirect');
 
 app.use(express.static(__dirname + '/app')); // set static files location, in this case the route, add a file name if not
 
 app.use(express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/node_modules'));
 app.use(express.static(__dirname));
+app.use(sslRedirect());
 
 /* At the top, with other redirect methods before other routes */
 app.get('*',function(req,res,next){
