defb76b1a5af2930b7dc921a1805fb9de753f78f
Rishabh Shukla <rishab.akg@gmail.com>
2020-06-27 18:59:25 +0530

    deploying to Heroku

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+app.get('/', function(req, res) {
+    res.sendfile('index.html', {root: __dirname })
+});
 app.listen(process.env.PORT || 3000);
